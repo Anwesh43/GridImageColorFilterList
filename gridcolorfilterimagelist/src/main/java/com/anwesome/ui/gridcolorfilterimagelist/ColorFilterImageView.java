8f0bf36674a6aefb2dd8ba627b6c1a85bde60618
Anwesh43 <anweshthecool0@gmail.com>
2017-05-07 06:54:35 +0530

    Adding ColorFilterImageView

---
+++ gridcolorfilterimagelist/src/main/java/com/anwesome/ui/gridcolorfilterimagelist/ColorFilterImageView.java
@@ -0,0 +1,37 @@
+package com.anwesome.ui.gridcolorfilterimagelist;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by anweshmishra on 07/05/17.
+ */
+public class ColorFilterImageView extends View {
+    private Bitmap bitmap;
+    private int color;
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private int render = 0,w,h;
+    public ColorFilterImageView(Context context,Bitmap bitmap,int color) {
+        super(context);
+        this.bitmap = bitmap;
+        this.color = color;
+    }
+    public void onDraw(Canvas canvas) {
+        if(render == 0) {
+            w = canvas.getWidth();
+            h = canvas.getHeight();
+            bitmap = Bitmap.createScaledBitmap(bitmap,w,h,true);
+        }
+        render++;
+    }
+    public boolean onTouchEvent(MotionEvent event) {
+        return true;
+    }
+    public void update(float factor) {
+
+    }
+}
